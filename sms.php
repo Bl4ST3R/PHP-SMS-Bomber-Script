091a93de8a737a4c3326e14b874b47a238d92da4
Dev <Bl4ST3R@users.noreply.github.com>
2018-01-01 16:06:11 +0530

    Create sms.php

---
+++ sms.php
@@ -0,0 +1,29 @@
+<?php
+  $no=$_GET['no'];
+   if( isset($_GET["no"]) ) {
+      echo "Phone no :". $_GET['no']. "<br />";
+      
+      for ($i=1; $i <= $_GET['count'] ; $i++) { 
+         $curl = curl_init();
+         curl_setopt_array($curl, array(
+    CURLOPT_RETURNTRANSFER => 1,
+    CURLOPT_URL => 'sitename?demophone=$no',
+    CURLOPT_USERAGENT => 'Test request'));
+    $resp = curl_exec($curl);
+    curl_close($curl); 
+      }
+      
+      exit();
+   }
+?>
+<html>
+   <body>
+   
+      <form action = "sms.php" method = "GET">
+         Phone no: <input type = "number" name = "no" />
+         count: <input type = "number" name = "count" value="1"/>
+         <input type = "submit" />
+      </form>
+      
+   </body>
+</html>
